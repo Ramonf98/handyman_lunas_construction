a0398ca4419c1fd8cb3a6eedd916a418ce212604
Ramón Cordova <51839009+Ramonf98@users.noreply.github.com>
2022-05-21 10:53:26 -0700

    Progress of Handyman Luna's Construction page

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// import { allTranslations } from './translations.js';
+
+// let currentPageLang = 'en';
+
+// document.getElementById('lang_picker').addEventListener('click', () => {
+//     currentPageLang = currentPageLang == 'en' ? 'es' : 'en';
+
+//     const translations = allTranslations[currentPageLang];
+//     const translationKeys = Object.keys(translations);
+
+//     translationKeys.forEach((elementID) => {
+//         console.log(elementID);
+//         const translatedText = translations[elementID];
+//         const htmlElement = document.getElementById(elementID);
+//         if (!htmlElement) return;
+//         htmlElement.innerHTML = translatedText;
+//     });
+// });
+
+// console.log(screen.width);
---
+++ js/translations.js
@@ -0,0 +1,19 @@
+const allTranslations = {
+    en: {
+        lang_picker: 'English',
+        about_us: 'About Us',
+        our_projects: 'Projects',
+        contact: 'Contact',
+        title_about_us: 'About <span class="color__blue title">Us</span>'
+    },
+
+    es: {
+        lang_picker: 'Español',
+        about_us: 'Nostros',
+        our_projects: 'Proyectos',
+        contact: 'Contacto',
+        title_about_us: 'Sobre <span class="color__blue title">Nosotros</span>'
+    }
+};
+
+export { allTranslations };
