ac54ddb60ed9eade2c45c0bbf1ed416c6d48d3c1
Ramón Cordova <51839009+Ramonf98@users.noreply.github.com>
2022-06-01 19:44:41 -0700

    Updated Translations

--- js/translations.js
+++ js/translations.js
@@ -6,7 +6,9 @@ const allTranslations = {
         nav_item_text_contact: 'Contact',
         title_about_us: 'About <span class="color__blue">Us</span>',
         title_our_projects: '<span class="color__blue">Our</span> Projects',
-        title_contact_us: 'Contact <span class="color__blue">Us</span>'
+        title_contact_us: 'Contact <span class="color__blue">Us</span>',
+        our_projects_section_description:
+            'We love all what we build for our customers. Getting their ideas to come true through our experience. You can find some of our adventures down below.'
     },
 
     es: {
@@ -16,7 +18,9 @@ const allTranslations = {
         nav_item_text_contact: 'Contacto',
         title_about_us: 'Sobre <span class="color__blue">Nosotros</span>',
         title_our_projects: '<span class="color__blue">Nuestros</span> Proyectos',
-        title_contact_us: 'Info. de <span class="color__blue">Contacto</span>'
+        title_contact_us: 'Info. de <span class="color__blue">Contacto</span>',
+        our_projects_section_description:
+            'Nos encanta todo lo que construimos para nuestros clientes. Consiguiendo hacer realidad sus ideas a través de nuestra experiencia. Puedes encontrar algunas de nuestras aventuras a continuación.'
     }
 };
 
