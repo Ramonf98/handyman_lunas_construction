adf099e0f7f9aba30baf9f7be16cb54f88a416c0
Ramón Cordova <51839009+Ramonf98@users.noreply.github.com>
2022-08-27 18:29:17 -0700

    Add files via upload

--- js/translations.js
+++ js/translations.js
@@ -8,7 +8,7 @@ const allTranslations = {
         title_our_projects: '<span class="color__blue">Our</span> Projects',
         title_contact_us: 'Contact <span class="color__blue">Us</span>',
         our_projects_section_description:
-            'We love all what we build for our customers. Getting their ideas to come true through our experience. You can find some of our adventures down below.'
+            "We're always ready to help! Feel free to check out part of our days in this section. You can consult our contact info more below."
     },
 
     es: {
@@ -20,7 +20,7 @@ const allTranslations = {
         title_our_projects: '<span class="color__blue">Nuestros</span> Proyectos',
         title_contact_us: 'Info. de <span class="color__blue">Contacto</span>',
         our_projects_section_description:
-            'Nos encanta todo lo que construimos para nuestros clientes. Consiguiendo hacer realidad sus ideas a través de nuestra experiencia. Puedes encontrar algunas de nuestras aventuras a continuación.'
+            '¡Siempre estamos listo para ayudar! Sientase libre de consultar parte de nuestro día a día en ésta seccion. Puede consultar nuestra información de contacto mas abajo.'
     }
 };
 
